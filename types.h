115f50a1ec2f325089a2e20c04feadb338db1e98
airforcetwo <airforcetwo@sina.com>
2014-08-06 16:09:20 +0800

    1.得到更新价格消息后，读取调价单文件，更新数据库。

---
+++ types.h
@@ -0,0 +1,14 @@
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+enum
+{
+    TYPES_RESTOCK       = 1,    //进货
+    TYPES_UPDATE_PRICE  = 2,    //调价
+    TYPES_INVENTORY     = 3,    //盘点
+    TYPES_LOSS          = 4,    //报损
+     
+
+};
+
+#endif __TYPES_H__
